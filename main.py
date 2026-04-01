d2158a04a96118ede4cdab60f19b433e43eb22ae
Елена Алябьева <aksenovaea@mail.ru>
2026-04-02 01:46:18 +0300

    GitHub Desktop

--- main.py
+++ main.py
@@ -7,13 +7,12 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print('Hello from GitHub Desktop')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 print('Hello, PyCharm!')
-print('Hello from GitHub Desktop')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
